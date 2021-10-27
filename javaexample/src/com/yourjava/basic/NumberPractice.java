8ef23677b67b481cd04575581565ad79e7a5d5bb
ngtang <ngoctangtkim@gmail.com>
2021-10-27 21:33:18 +0700

    NMKTLT

--- javaexample/src/com/yourjava/basic/NumberPractice.java
+++ javaexample/src/com/yourjava/basic/NumberPractice.java
@@ -1,23 +1,22 @@
 package com.yourjava.basic;
 
 public class NumberPractice {
+    static int number=0;
+    String text="supper";
 
-    public static void main(String[] agrs) {
-        NumberPractice numberPractice= new NumberPractice();
-        numberPractice.printAny("Hello world");
-    }
-
-    public void printAny(String text){
+    public void printAny(String text) {
         System.out.println(text);
+        System.out.println(this.number);
     }
 
 }
-// Project in java
-// Data Type|Variables (scope)
-// Static
-// Class vs Object
-// This|Super
-// Constructor
-// Access modifier and Encapsulation
-// Overriding, overloading
-// Inheritance, supper
+
+class A extends NumberPractice{
+    String text="child";
+    A(){
+        System.out.println(super.text);
+    }
+    public static void main(String[] agrs) {
+        A test= new A();
+    }
+}
